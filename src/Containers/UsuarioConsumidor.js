0d5bf8e5a7e5a3681d54aad9778978e81754426c
DavidCardonaG <softwarewd@outlook.com>
2021-10-04 16:23:52 -0500

    files

---
+++ src/Containers/UsuarioConsumidor.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+import Profesionales from '../Components/Profesionales'
+
+const UsuarioConsumidor = () => {
+    return (
+        <div>
+            <NavBar />
+            <Profesionales />
+        </div>
+    )
+}
+
+export default UsuarioConsumidor
+   
